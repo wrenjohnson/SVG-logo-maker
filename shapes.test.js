9f8ba8774558cbdab1385c934026cfc7e487dba1
wrenjohnson <wrenjohnson22@gmail.com>
2023-08-08 20:33:12 -0400

    initial commit

---
+++ shapes.test.js
@@ -0,0 +1,28 @@
+// This is a test suite run by "npm test" that checks to see if SVG creation conditions meet ideal conditions
+
+const {Circle, Square, Triangle} = require("../lib/shapes")
+
+describe('Circle', () => {
+    test('Renders correctly', () => {
+        const shape = new Circle();
+        var color =('blue')
+        shape.setColor(color);
+        expect(shape.render()).toEqual(`<circle cx="50%" cy="50%" r="100" fill="${color}" />`);
+    });
+  });
+  describe('Square', () => {
+      test('Renders correctly', () => {
+        const shape = new Square();
+        var color =('green')
+        shape.setColor(color);
+        expect(shape.render()).toEqual(`<rect x="90" y="40" width="200" height="200" fill="${color}" />`);
+      });
+    });
+  describe('Triangle', () => {
+      test('Renders correctly', () => {
+        const shape = new Triangle();
+        var color =('yellow')
+        shape.setColor(color);
+        expect(shape.render()).toEqual(`<polygon points="0,200 300,200 150,0" fill="${color}" />`);
+      });
+    });
